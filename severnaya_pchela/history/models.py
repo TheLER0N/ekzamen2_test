345b00d11f78c9d2acdc653b949ff861d5bb9e7d
LERON <164945360+TheLER0N@users.noreply.github.com>
2026-07-01 23:16:05 +0300

    update

--- severnaya_pchela/history/models.py
+++ severnaya_pchela/history/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
 class History(models.Model):
-    IdHistory = models.ForeignKey("guest.Guest", on_delete=models.CASCADE)
-    GuestId = models.IntegerField(null=True)
+    IdHistory = models.IntegerField(primary_key=True)
+    GuestId = models.ForeignKey("guest.Guest", on_delete=models.CASCADE)
     RoomId = models.ForeignKey("room.Room", on_delete=models.CASCADE)
     CheckIn = models.DateField(null=True)
     ChecklnOut = models.DateField(null=True)
