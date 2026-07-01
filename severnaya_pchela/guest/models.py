7d129592a3f865fc188855573e0f2b438f6a8182
LERON <164945360+TheLER0N@users.noreply.github.com>
2026-07-01 22:35:17 +0300

    update

--- severnaya_pchela/guest/models.py
+++ severnaya_pchela/guest/models.py
@@ -3,7 +3,6 @@ from django.db import models
 class Guest(models.Model):
     idGuest = models.IntegerField(primary_key=True)
     FullName = models.TextField(verbose_name="Полное имя")
-    Email = models.EmailField()
     Birthday = models.DateField(verbose_name="День рождение")
     GenderId = models.ForeignKey("gender.Gender", on_delete=models.CASCADE, verbose_name= "пол")
     StatusId = models.ForeignKey("status.Status", on_delete=models.CASCADE, verbose_name="Статус")
